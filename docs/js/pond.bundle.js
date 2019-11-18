670fd3691ca90d119dbdc918dbba0aaf34f478a8
Aidan Smithers <porrige51122>
2019-11-18 16:25:26 +0000

    Removed Scroll

--- docs/js/pond.bundle.js
+++ docs/js/pond.bundle.js
@@ -2330,9 +2330,9 @@ exports.default = Water;
   !*** ./src/mystyles.scss ***!
   \***************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-// extracted by mini-css-extract-plugin
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\r\n  overflow: hidden !importan;\r\n                  ^\r\n      Invalid CSS after \"  overflow: hidden\": expected expression (e.g. 1px, bold), was \"!importan;\"\r\n      in C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\src\\mystyles.scss (line 12, column 20)\n    at runLoaders (C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\webpack\\lib\\NormalModule.js:302:20)\n    at C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.render [as callback] (C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\sass-loader\\lib\\loader.js:52:13)\n    at Object.done [as callback] (C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\neo-async\\async.js:8067:18)\n    at options.error (C:\\Users\\Aidan\\OneDrive - Royal Holloway University of London\\Personal\\Projects\\Pond\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 
---
+++ src/movement/spatialHash.js
@@ -0,0 +1,8 @@
+class SpatialHash {
+  constructor(cellSize) {
+    this.cells = [][];
+    this.cellSize = cellSize;
+  }
+}
+
+export defualt SpatialHash;
